8f19f2ac67bcf25883778b2bfcf229f51c87f28d
Silviu Iosif <6332838@rts-365.dk>
2021-01-14 10:24:21 +0100

    adding virtual reality branch

---
+++ src/components/virtual.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Virtual(){
+    return (
+        <article>
+           <h2>Virtual Reality</h2>
+           <img src="./icon/bookmark.svg" alt="bookmark"/>
+           <img src="./images/virtual.png" alt="virtual reality"/>
+           <h4>TECHNOLOGY</h4>
+        </article>
+        )
+}
+
+export default Virtual;
